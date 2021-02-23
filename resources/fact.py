f93f1e669d4510353ed3697001696116ffcb7d48
neua-wd <neua.worabodee@gmail.com>
2021-02-23 13:49:03 +0000

    Takes the whole fact for updates

--- resources/fact.py
+++ resources/fact.py
@@ -5,19 +5,18 @@ from constants import TABLES_DIRECTORY
 
 class Fact(Resource):
   parser = reqparse.RequestParser()
-  parser.add_argument('fact_id', type=str, required=True, help='Please provide the fact id')
-  parser.add_argument('column', type=str, required=True, help='Please provide the column')
-  parser.add_argument('new_text', type=str, required=True, help='Please provide the new_text')
+  parser.add_argument('edited_fact', type=dict, required=True, help='Please provide the edited fact')
 
   def put(self):
-    fact_id = Fact.parser.parse_args()['fact_id']
-    column = Fact.parser.parse_args()['column']
-    new_text = Fact.parser.parse_args()['new_text']
+    edited_fact = Fact.parser.parse_args()['edited_fact']
+    fact_id = edited_fact['[SKIP] UID']
 
     for path in glob.glob(TABLES_DIRECTORY + '/*.tsv'):
             table = pd.read_csv(path, sep='\t')
             if(fact_id in table['[SKIP] UID'].values):
-              table.loc[table['[SKIP] UID'] == fact_id, column] = new_text
+              # row = table.loc[table['[SKIP] UID'] == fact_id]
+              for column in edited_fact:
+                table.loc[table['[SKIP] UID'] == fact_id, column] = edited_fact[column]
               table.to_csv(path, sep='\t', index=False)
 
     return 'success'
