05dcc039c50e91203ec9433b7b2d88965f7a6ab1
neua-wd <neua.worabodee@gmail.com>
2021-02-25 15:55:55 +0000

    As a user, I can add a new fact

--- resources/fact.py
+++ resources/fact.py
@@ -3,27 +3,24 @@ import glob
 import pandas as pd
 import uuid
 from constants import TABLES_DIRECTORY
+from constants import QUESTION_FILE_PATH
 
 class Fact(Resource):
   parser = reqparse.RequestParser()
   parser.add_argument('edited_fact', type=dict, help='Please provide the edited fact')
   parser.add_argument('table_name', type=str, help='Please provide the table name')
+  parser.add_argument('to_question', type=str, help='Please provide the question ID')
   parser.add_argument('new_fact', type=dict, help='Please provide the new fact')
 
   def post(self):
-    table_name = Fact.parser.parse_args()['table_name']
+    fact_table_name = Fact.parser.parse_args()['table_name']
+    question_id = Fact.parser.parse_args()['to_question']
     new_fact = Fact.parser.parse_args()['new_fact']
-    table_path = TABLES_DIRECTORY + "/" + table_name + '.tsv'
-    table = pd.read_csv(table_path, sep='\t')
+    fact_table_path = TABLES_DIRECTORY + "/" + fact_table_name + '.tsv'
+    new_fact_id = str(uuid.uuid4())
 
-    new_row = {}
-    for column_name in new_fact:
-      new_row[column_name] = new_fact[column_name]
-    
-    new_row['[SKIP] UID'] = uuid.uuid4()
-
-    updated_table = table.append(new_row, ignore_index=True)
-    updated_table.to_csv(table_path, sep='\t', index=False)
+    self.__addNewFact(fact_table_path, new_fact, new_fact_id)
+    self.__addFactToExplanation(question_id, new_fact_id)
 
     return 'success'
 
@@ -47,3 +44,24 @@ class Fact(Resource):
             table = pd.read_csv(path, sep='\t')
             return table.loc[table['[SKIP] UID'] == fact_id]
             
+
+  # Add the new fact to the corresponding fact table
+  def __addNewFact(self, fact_table_path, new_fact, new_fact_id):
+    fact_table = pd.read_csv(fact_table_path, sep='\t')
+
+    new_row = {}
+    for column_name in new_fact:
+      new_row[column_name] = new_fact[column_name]
+    new_row['[SKIP] UID'] = new_fact_id
+    updated_fact_table = fact_table.append(new_row, ignore_index=True)
+
+    updated_fact_table.to_csv(fact_table_path, sep='\t', index=False)
+
+
+  # Add the id of the new fact to the list of facts for the question's explanation
+  def __addFactToExplanation(self, question_id, new_fact_id):
+    questions_table = pd.read_csv(QUESTION_FILE_PATH, sep='\t')
+    question_row = questions_table[questions_table['QuestionID'] == question_id]
+    questions_table.loc[questions_table['QuestionID'] == question_id, 'explanation'] = question_row['explanation'].values[0] + ' ' + new_fact_id
+
+    questions_table.to_csv(QUESTION_FILE_PATH, sep='\t', index=False)
